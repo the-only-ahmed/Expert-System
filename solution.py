51ae80e8c30145ef60be2145fa8006024cfaf495
U-pc\pcv <ael-kadh@student.42.fr>
2015-12-25 22:19:58 +0100

    implementation almost done

--- solution.py
+++ solution.py
@@ -1,13 +1,15 @@
 from parser import *
 
 def solve(variables, rules, queries):
-    for rule in rules:
-        if len([item for item in rule if item[1] == IMP]) > 0:
-            if solveRule(rule):
-                rules.remove(rule)
-        else:
-            print IFOF + " is currently not available"
-            exit()
+    while len(rules) > 0 and checkQuery():
+        for rule in rules:
+            if len([item for item in rule if item[1] == IMP]) > 0:
+                if solveRule(rule):
+                    rules.remove(rule)
+            else:
+                print IFOF + " is currently not available"
+                exit()
+    print queries
 
 def solveRule(rule):
     isImp = False
@@ -31,40 +33,64 @@ def solveRule(rule):
 
 def setOtherSide(sideLst, left):
     unknownLst = [item for item in sideLst if item[1] == VAR and variables[item[0]] == None]
-    if len(unknownLst) > 1:
+    if len(unknownLst) != 1:
         return None
     unknown = unknownLst[0][0]
-    print unknown + " in " + str(sideLst)
+    variables[unknown] = solveSide(sideLst, left)
+    queries[unknown] = variables[unknown]
+    return True
 
-def solveSide(sideLst):
+def solveSide(*args):
     op = None
     var = None
     no = False
     res = None
 
-    for item in sideLst:
-        if item[1] == NOT:
-            no = True
-        elif item[1] == OP:
-            op = item[0]
-        elif item[1] == VAR:
-            if variables[item[0]] is None:
-                return None
-            elif res is None:
-                v = variables[item[0]]
-                if no:
-                    v = not v
-                    no = False
-                res = v
-            else:
-                v = variables[item[0]]
-                if no:
-                    v = not v
-                    no = False
-                if op == '+':
-                    res = res and v
-                elif op == '|':
-                    res = res or v
-                else:
-                    res = res ^ v
+    supp = [True, False]
+    wantedRes = None
+    i = 0
+
+    sideLst = args[0]
+    if len(args) == 2:
+        wantedRes = args[1]
+
+    try :
+        for i in range(len(args)):
+            for item in sideLst:
+                if item[1] == NOT:
+                    no = True
+                elif item[1] == OP:
+                    op = item[0]
+                elif item[1] == VAR:
+                    v = variables[item[0]]
+                    if v is None:
+                        if wantedRes is not None:
+                            v = supp[i]
+                    if res is None:
+                        if no:
+                            v = not v
+                            no = False
+                        res = v
+                    else:
+                        if no:
+                            v = not v
+                            no = False
+                        if op == '+':
+                            res = res and v
+                        elif op == '|':
+                            res = res or v
+                        else:
+                            res = res ^ v
+            if wantedRes is not None and res == wantedRes:
+                res = supp[i]
+                break
+            i += 1
+    except:
+        return None
     return res
+
+def checkQuery():
+    for q in queries:
+        if queries[q] is None:
+            return True
+    return False
